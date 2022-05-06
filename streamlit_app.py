4bb0f6310c9df66720ebb99fd281d4c0e0732ec3
Daniel M. Smith <danielmsmith1@gmail.com>
2022-05-06 16:01:09 -0400

    Update streamlit_app.py
    fruityapi add

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,12 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#NEW SECTIOn to display fruityapi response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,3 +23,5 @@ fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+
